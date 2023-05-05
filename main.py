baa423e4b6e01bd5a41ed267afee032d0c676f64
Rail <railbolo@gmail.com>
2023-05-05 21:44:43 +0400

    add function menu, read, save, show

---
+++ main.py
@@ -0,0 +1,132 @@
+# Напишите проект, содержащий функционал работы с заметками.
+# Программа должна уметь создавать заметку, сохранять её,
+# читать список заметок, редактировать заметку, удалять заметку.
+
+import os
+import json
+import datetime
+
+
+file_notes = 'notes.json'
+
+
+# Функция чтения записей из файла JSON
+
+
+def read_notes(file):
+    with open(file, 'r', encoding='utf-8') as data:
+        try:
+            notes = json.load(data)
+        except json.decoder.JSONDecodeError as e:
+            return None
+    return notes
+
+
+# Функция создания записи
+
+
+def add_note(notes):
+    if not notes:
+        id = 1
+        notes = []
+    else:
+        id = len(notes) + 1
+    title = input('Введите имя записи: ')
+    text = input('Введите текст заметки: ')
+    time_marker = datetime.datetime.now().strftime('%d-%m-%Y %H:%M')
+    note = {'ID': id, 'TITLE': title, 'TEXT': text, 'TIME': time_marker}
+    notes.append(note)
+    save_note = input(
+        'Сохранить запись? 1 - да, 0 - нет\n')
+    while True:
+        if not save_note.isdigit:
+            print('Введите 0 или 1')
+        else:
+            if save_note == "1":
+                save_notes(notes)
+                break
+            else:
+                break
+    return notes
+
+# Функция показа записей
+
+
+def show_notes(notes):
+    if not notes:
+        print('Записей пока нет')
+        input('\n"Enter - возврат в меню >> ')
+        menu()
+    else:
+        for note in notes:
+            print(f'ID: {note["id"]}')
+            print(f'TITLE: {note["title"]}')
+            print(f'TEXT: {note["text"]}')
+            print(f'TIME: {note["time_marker"]}')
+            print('_________________')
+
+
+# Функция записи в файл JSON
+
+def save_notes(note):
+    with open(file_notes, 'a', encoding='utf-8') as data:
+        json.dump(note, data, ensure_ascii=True)
+
+
+# Функция меню
+
+
+def menu():
+    os.system("cls")
+    menu = ('Добро пожаловать в программу "Блокнот"\n\n'
+            'Доступные действия:\n'
+            '1 - Показать все заметки\n'
+            '2 - Добавить заметку\n'
+            '3 - Удалить заметку\n'
+            '4 - Найти заметку\n'
+            '5 - Редактировать заметку\n'
+            '6 - Сохранить заметки\n'
+            '7 - Выход')
+    print(menu)
+    notes = read_notes(file_notes)
+    fail_answer = 5
+    while fail_answer > 0:
+        answer = input('Введите номер действия:>> ')
+        if not answer.isdigit():
+            print('Нужно ввести число от 1 до 7')
+            fail_answer -= 1
+            continue
+        else:
+            if answer == '1':
+                os.system("cls")
+                show_notes(notes)
+                print(menu)
+            if answer == '2':
+                os.system("cls")
+                add_note(notes)
+                print(menu)
+            if answer == '3':
+                os.system("cls")
+                delete_note()
+                print(menu)
+            if answer == '4':
+                os.system("cls")
+                search_note()
+                print(menu)
+            if answer == '5':
+                os.system("cls")
+                edit_note()
+                print(menu)
+            if answer == '6':
+                os.system("cls")
+                save_notes()
+                print(menu)
+            if answer == '7':
+                exit(0)
+
+    print('Похоже, вы делаете что-то не так((')
+    exit(0)
+
+
+if __name__ == '__main__':
+    menu()
